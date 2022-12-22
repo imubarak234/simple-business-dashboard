bb2d63040021659d98c5103ce5c414325966fdd5
imubarak234 <imubarak2424@gmail.com>
2022-12-22 15:05:02 +0100

    topbar components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { ColorModeContext, useMode } from './theme';
+import Topbar from './scenes/global/Topbar';
 
 function App() {
+  const [theme, colorMode] = useMode();
+
   return (
-    <div className="app">
-      
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
