be0172121d942e592071f43ab3eb8cca730ce080
imubarak234 <imubarak2424@gmail.com>
2022-12-24 00:53:50 +0100

    side bar

--- src/scenes/global/Sidebar.js
+++ src/scenes/global/Sidebar.js
@@ -1,3 +1,51 @@
-const Sidebar = () => <div>Sidebar</div>;
+import { useState } from "react";
+import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
+import 'react-pro-sidebar/dist/css/style.css';
+import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { Link } from "react-router-dom";
+import { tokens } from "../../theme";
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import PeopleOutlinedIcon from '@mui/icons-material/PeopleOutlined';
+import ContactsOutlinedIcon from '@mui/icons-material/ContactsOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined';
+import HelpOutlinedIcon from '@mui/icons-material/HelpOutlined';
+import BarChartOutlinedIcon from '@mui/icons-material/BarChartOutlined';
+import PieChartOutlineOutlinedIcon from '@mui/icons-material/PieChartOutlineOutlined';
+import TimelineOutlinedIcon from '@mui/icons-material/TimelineOutlined';
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
+
+
+const Sidebar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [ isCollapsed, setIsCollapsed ] = useState(false);
+  const [ selected, setSelected ] = useState("Dashboard");
+
+  return (
+    <Box
+    sx={{
+      "& .pro-sidebar-inner": {
+        background: `${colors.primary[400]} !important`
+      },
+      "& .pro-icon-wrapper" : {
+        backgroundColor: "transparent !important"
+      },
+      "& .pro-inner-item": {
+        paddding: "5px 35px 5px 20px !important"
+      },
+      "& .pro-inner-item:hover": {
+        color: "#868dfb !important"
+      },
+      "& .pro-menu-item.active": {
+        color: "#6870fa !important"
+      }
+    }}
+    >
+
+    </Box>
+  );
+}
 
 export default Sidebar;
