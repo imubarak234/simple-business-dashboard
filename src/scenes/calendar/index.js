57ce0b1795c871b43faef145d845aef5387e0dbb
imubarak234 <imubarak2424@gmail.com>
2022-12-26 15:22:06 +0100

    calendar so far

---
+++ src/scenes/calendar/index.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import FullCalendar, { formatDate } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/list';
+import listPlugin from '@fullcalendar/list';
+import interactionPlugin from '@fullcalendar/interaction';
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  useTheme
+} from '@mui/material';
+import Header from "../../components/Header";
+import { tokens } from "../../theme";
+
+const Calendar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const [ currentEvents, setCurrentEvents ] = useState([]);
+
+  const handleDateClick = (selected) => {
+    const title = prompt('Please enter a new title for your event');
+    const calendarApi = selected.view.calendar;
+    calendarApi.unselect();
+
+    if (title) {
+      calendarApi.addEvent({
+        id: `${selected.dateStr}-${title}`,
+        title,
+        start: selected.startStr,
+        end: selected.endStr,
+        allDay: selected.allDay,
+      });
+    };
+  };
+
+  const handleEventClick = (selected) => {
+    if (window.confirm(`Are you sure you want to delete the event '${selected.event.title}'`)) {
+      selected.event.remove();
+    }
+  };
+
+  return (
+    <Box m='20px'>
+      <Header title="CALENDAR" subtitle="Full Calendar Interactive Page" />
+
+      <Box
+      display='flex'
+      justifyContent='space-between'
+      ></Box>
+    </Box>
+  )
+}
+
+export default Calendar;
--- src/scenes/form/index.js
+++ src/scenes/form/index.js
@@ -2,35 +2,36 @@
 /* eslint-disable no-console */
 
 import { Box, Button, TextField } from '@mui/material';
-import { Formik } from 'formik';
+import { Form, Formik } from 'formik';
 import * as yup from 'yup';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import Header from '../../components/Header';
 
-const initialValues = {
-  firstName: '',
-  lastName: '',
-  email: '',
-  contact: '',
-  address1: '',
-  address2: '',
-};
+const ProfileForm = () => {
+  const initialValues = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    contact: '',
+    address1: '',
+    address2: '',
+  };
 
-const phoneRegExp = /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0,9]{3,4}$/;
+  // const phoneRegExp =
+  // /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0,9]{3,4}$/;
 
-const userSchema = yup.object().shape({
-  firstName: yup.string().required('required'),
-  lastName: yup.string().required('required'),
-  email: yup.string().email('Invalid Email').required('required'),
-  contact: yup
-    .string()
-    .matches(phoneRegExp, 'Phone number is not valid')
-    .required('required'),
-  address1: yup.string().required('required'),
-  address2: yup.string().required('required'),
-});
+  const userSchema = yup.object().shape({
+    firstName: yup.string().required('required'),
+    lastName: yup.string().required('required'),
+    email: yup.string().email('Invalid Email').required('required'),
+    contact: yup
+      .string()
+      // .matches(phoneRegExp, 'Phone number is not valid')
+      .required('required'),
+    address1: yup.string().required('required'),
+    address2: yup.string().required('required'),
+  });
 
-const Form = () => {
   const isNonMobile = useMediaQuery('(min-width:600px)');
 
   const handleFormSubmit = (values) => {
@@ -69,7 +70,7 @@ const Form = () => {
                 value={values.firstName}
                 name="firstName"
                 error={!!touched.firstName && !!errors.firstName}
-                helperText={touched.firstName && !!errors.firstName}
+                helperText={touched.firstName && errors.firstName}
                 sx={{ gridColumn: 'span 2' }}
               />
               <TextField
@@ -82,7 +83,7 @@ const Form = () => {
                 value={values.lastName}
                 name="lastName"
                 error={!!touched.lastName && !!errors.lastName}
-                helperText={touched.lastName && !!errors.lastName}
+                helperText={touched.lastName && errors.lastName}
                 sx={{ gridColumn: 'span 2' }}
               />
               <TextField
@@ -95,7 +96,7 @@ const Form = () => {
                 value={values.email}
                 name="email"
                 error={!!touched.email && !!errors.email}
-                helperText={touched.email && !!errors.email}
+                helperText={touched.email && errors.email}
                 sx={{ gridColumn: 'span 4' }}
               />
               <TextField
@@ -108,7 +109,7 @@ const Form = () => {
                 value={values.contact}
                 name="contact"
                 error={!!touched.contact && !!errors.contact}
-                helperText={touched.contact && !!errors.contact}
+                helperText={touched.contact && errors.contact}
                 sx={{ gridColumn: 'span 4' }}
               />
               <TextField
@@ -121,7 +122,7 @@ const Form = () => {
                 value={values.address1}
                 name="address1"
                 error={!!touched.address1 && !!errors.address1}
-                helperText={touched.address1 && !!errors.address1}
+                helperText={touched.address1 && errors.address1}
                 sx={{ gridColumn: 'span 4' }}
               />
               <TextField
@@ -134,10 +135,19 @@ const Form = () => {
                 value={values.address2}
                 name="address2"
                 error={!!touched.address2 && !!errors.address2}
-                helperText={touched.address2 && !!errors.address2}
+                helperText={touched.address2 && errors.address2}
                 sx={{ gridColumn: 'span 4' }}
               />
 
+            </Box>
+            <Box
+              display="flex"
+              justifyContent="end"
+              mt="20px"
+            >
+              <Button type="submit" color="secondary" variant="contained">
+                Create New User
+              </Button>
             </Box>
           </Form>
         )}
@@ -146,4 +156,4 @@ const Form = () => {
   );
 };
 
-export default Form;
+export default ProfileForm;
