3de3cb519082cd75c77ce44010df218973d372d5
imubarak234 <imubarak2424@gmail.com>
2022-12-27 02:20:31 +0100

    bar page

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Contacts from './scenes/contacts';
 import Calendar from './scenes/calendar';
 import ProfileForm from './scenes/form';
 import FAQ from './scenes/faq';
-// import Bar from './scenes/bar';
+import Bar from './scenes/bar';
 // import Line from './scenes/line';
 // import Pie from './scenes/pie';
 // import Geography from './scenes/geography';
@@ -34,7 +34,7 @@ function App() {
               <Route path="/form" element={<ProfileForm />} />
               <Route path="/calendar" element={<Calendar />} />
               <Route path="/faq" element={<FAQ />} />
-              {/* <Route path="/bar" element={<Bar />} /> */}
+              <Route path="/bar" element={<Bar />} />
               {/* <Route path="/pie" element={<Pie />} /> */}
               {/* <Route path="/line" element={<Line />} /> */}
               {/* <Route path="/geography" element={<Geography />} /> */}
