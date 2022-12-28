8e1d22edf13677d5765146d28490451935a5f806
imubarak234 <imubarak2424@gmail.com>
2022-12-28 12:28:41 +0100

    geography complete

--- src/scenes/geography/index.js
+++ src/scenes/geography/index.js
@@ -1,14 +1,24 @@
-import { Box } from '@mui/material';
+import { Box, useTheme } from '@mui/material';
 import Header from '../../components/Header';
 import GeographyChart from '../../components/GeographyChart';
+import { tokens } from '../../theme';
 
-const Geography = () => (
-  <Box m="20px">
-    <Header title="Geography Chart" subtitle="Simple Geography Cart" />
-    <Box height="75vh">
-      <GeographyChart />
+const Geography = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+  return (
+    <Box m="20px">
+      <Header title="Geography Chart" subtitle="Simple Geography Cart" />
+      <Box
+        height="75vh"
+        border={`1px solid ${colors.grey[100]}`}
+        borderRadius="4px"
+      >
+        <GeographyChart />
+      </Box>
     </Box>
-  </Box>
-);
+  );
+};
 
 export default Geography;
