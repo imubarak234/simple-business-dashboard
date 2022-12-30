d0cb3d8d3bc3fdf487a7dba4595a310170d31dc2
imubarak234 <imubarak2424@gmail.com>
2022-12-30 15:45:19 +0100

    linter errors

--- src/components/progressCircle.js
+++ src/components/progressCircle.js
@@ -1,24 +1,29 @@
-import { Box, useTheme } from "@mui/material";
-import { tokens } from "../theme";
+import { Box, useTheme } from '@mui/material';
+import PropTypes from 'prop-types';
+import { tokens } from '../theme';
 
-const ProgressCirlce = ({ progress = "0.75", size = "40" }) => {
-
+const ProgressCirlce = ({ progress = '0.75', size = '40' }) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const angle = progress * 360;
 
   return (
     <Box
-    sx={{
-      background: `radial-gradient(${colors.primary[400]}) 55%, transparent 56%,
+      sx={{
+        background: `radial-gradient(${colors.primary[400]}) 55%, transparent 56%,
       conic-gradient(transparent 0deg ${angle}deg, ${colors.blueAccent[500]} ${angle}deg 360deg)
       ${colors.greenAccent[500]}`,
-      borderRadius: "50%",
-      width: `${size}px`,
-      height: `${size}px`,
-    }} 
+        borderRadius: '50%',
+        width: `${size}px`,
+        height: `${size}px`,
+      }}
     />
-  )
-}
+  );
+};
+
+ProgressCirlce.propTypes = {
+  progress: PropTypes.string.isRequired,
+  size: PropTypes.string.isRequired,
+};
 
 export default ProgressCirlce;
