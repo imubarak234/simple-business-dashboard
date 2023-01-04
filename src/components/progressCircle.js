1c9b9f31c717bb5a68b0f8b73f7b617f3af1ad46
imubarak234 <imubarak2424@gmail.com>
2023-01-04 17:20:14 +0100

    updates

--- src/components/progressCircle.js
+++ src/components/progressCircle.js
@@ -2,7 +2,7 @@ import { Box, useTheme } from '@mui/material';
 import PropTypes from 'prop-types';
 import { tokens } from '../theme';
 
-const ProgressCirlce = ({ progress = '0.75', size = '40' }) => {
+const ProgressCircle = ({ progress = '0.75', size = '40' }) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const angle = progress * 360;
@@ -10,8 +10,8 @@ const ProgressCirlce = ({ progress = '0.75', size = '40' }) => {
   return (
     <Box
       sx={{
-        background: `radial-gradient(${colors.primary[400]}) 55%, transparent 56%,
-      conic-gradient(transparent 0deg ${angle}deg, ${colors.blueAccent[500]} ${angle}deg 360deg)
+        background: `radial-gradient(${colors.primary[400]} 55%, transparent 56%),
+      conic-gradient(transparent 0deg ${angle}deg, ${colors.blueAccent[500]} ${angle}deg 360deg),
       ${colors.greenAccent[500]}`,
         borderRadius: '50%',
         width: `${size}px`,
@@ -21,9 +21,13 @@ const ProgressCirlce = ({ progress = '0.75', size = '40' }) => {
   );
 };
 
-ProgressCirlce.propTypes = {
+ProgressCircle.propTypes = {
   progress: PropTypes.string.isRequired,
-  size: PropTypes.string.isRequired,
+  size: PropTypes.string,
 };
 
-export default ProgressCirlce;
+ProgressCircle.defaultProps = {
+  size: '40',
+};
+
+export default ProgressCircle;
